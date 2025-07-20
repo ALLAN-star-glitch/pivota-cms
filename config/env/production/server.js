277500dfd1df7227df111137fd594f99cff449bb
Allan Mathenge <allanmathenge67@gmail.com>
2025-07-20 12:20:13 +0300

    added console logs on server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,11 @@
-module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-});
+module.exports = ({ env }) => {
+  console.log("RENDER_EXTERNAL_URL:", env("RENDER_EXTERNAL_URL"));
+  console.log("APP_KEYS:", env("APP_KEYS"));
+
+  return {
+    url: env("RENDER_EXTERNAL_URL"),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+  };
+};
