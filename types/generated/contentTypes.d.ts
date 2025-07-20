b46efb6665198c73c0dd8fa32e4691b8e3bddc84
Allan Mathenge <allanmathenge67@gmail.com>
2025-07-20 12:34:30 +0300

    added field for the hero sections content type

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -427,6 +427,7 @@ export interface ApiHeroSectionHeroSection extends Struct.CollectionTypeSchema {
     > &
       Schema.Attribute.Private;
     publishedAt: Schema.Attribute.DateTime;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
